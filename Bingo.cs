54155075872e282b231ca158f8e7bf282d3e9309
upyopyo18 <upyopyo18@gmail.com>
2020-03-20 14:05:35 +0900

    4-3解答

---
+++ Bingo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace CS_Practice
+{
+    public class Bingo
+    {
+        public int size = 0;
+        public int[,] card;
+
+        //ランダムでカードを生成するメソッド
+        private void create_card()
+        {
+            this.card = new int[this.size, this.size];
+            Random rnd = new Random();
+            for (int i = 0; i < this.size * this.size; i++)
+            {
+                //ランダムな位置を指定し、まだ初期値であれば数字を入れる
+                while (true)
+                {
+                    int row = rnd.Next(0, this.size);
+                    int col = rnd.Next(0, this.size);
+
+                    if (this.card[row, col] == 0)
+                    {
+                        this.card[row, col] = i + 1;
+                        break;
+                    }
+                }
+            }
+        }
+
+        //ビンゴの表示を行うメソッド
+        public void Show()
+        {
+            this.create_card();
+            //最大値の桁数の計算
+            int digit = (this.size * this.size).ToString().Length;
+
+            for (int i = 0; i < this.size; i++)
+            {
+                for (int j = 0; j < this.size; j++)
+                {
+                    //最大の桁数に合わせて、各数字を空白埋めする。
+                    Console.Write("|{0}", this.card[i, j].ToString().PadLeft(digit,' '));
+                }
+                //行末の"|"の表示
+                Console.WriteLine("|");
+
+                //横線の数を桁数から計算して表示
+                for(int j = 0; j < this.size * (digit + 1) + 1; j++)
+                    Console.Write("-");
+                Console.WriteLine();
+
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CS_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bingo bingo = new Bingo();
+            Console.Write("ビンゴのマスの縦・横のサイズを入力：");
+            bingo.size = int.Parse(Console.ReadLine());
+            //MEMO: 要素の最大が４桁になる最小のサイズは32（√1000=31.622..なので）
+            //      ３桁になる最小サイズは単純に√100=10
+
+            bingo.Show();
+        }
+    }
+}
